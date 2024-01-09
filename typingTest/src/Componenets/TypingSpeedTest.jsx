00ef8f1c31d14cc7ffbbdcbb2b467aa21c35bbdf
Sibtain Iqbal <139392522+Sibtain-Iqbal@users.noreply.github.com>
2024-01-09 09:29:15 -0800

    type

--- typingTest/src/Componenets/TypingSpeedTest.jsx
+++ typingTest/src/Componenets/TypingSpeedTest.jsx
@@ -2,7 +2,11 @@ import React  from "react";
 import { useContext,createContext } from "react";
 
 
-export const list = createContext({})
+export const list = createContext({
+    type : [
+        
+    ]
+})
 
 export const setType = createContext({})
 
